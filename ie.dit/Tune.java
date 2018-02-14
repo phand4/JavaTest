ea39593f4d32659275435fbdad46624527212560
phand4 <33064407+phand4@users.noreply.github.com>
2018-02-14 16:22:33 +0000

    Tune.java added.

---
+++ ie.dit/Tune.java
@@ -0,0 +1,23 @@
+public class Tune
+{
+	private int x;
+	private string title;
+	private string altTitle;
+	private string notation;
+
+	public int getX(){
+		return x;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	public String getAltTitle(){
+		return altTitle;
+	}
+
+	public String getNotation(){
+		return notation;
+	}
+}
